d8d867b6775d88ec6ea68d3940286ead05daa919
AsuraBeliever <alansethmanjarrez@gmail.com>
2025-05-19 00:11:46 -0600

    Log_in.py is now set on a class

--- log_in.py
+++ log_in.py
@@ -1,60 +1,64 @@
 import instaloader
 import tkinter as tk
 
-def login():
-    username = username_box.get()
-    password = password_box.get()
-    try:
-        L.load_session_from_file(username)
-    except FileNotFoundError:
-        try:
-            L.login(username, password) # Should prompt for 2FA
-            L.save_session_to_file()
-        except instaloader.exceptions.TwoFactorAuthRequiredException:
-            code = twoFactorAuthCode()
-            L.two_factor_login(code)
-            L.save_session_to_file()
-        
-    showSuccessWindow()
-
-def twoFactorAuthCode():
-    code = None
-    auth_window = tk.Toplevel(login_window)
-    auth_window.title("Two Factor Authentication")
-    auth_window.geometry("250x300")
-    tk.Label(auth_window, text="Enter code").pack()
-    code_box = tk.Entry(auth_window)
-    code_box.pack()
-    def getCode():
-        nonlocal code
-        code = code_box.get()
-        auth_window.destroy()
+class InstagramAccount:
+    def __init__(self, title : str, resolution : str):
+        self.L = instaloader.Instaloader()
+        self.login_window = tk.Tk()
+        self.login_window.title(title)
+        self.login_window.geometry(resolution)
     
-    tk.Button(auth_window, text="Send", command=getCode).pack()
-    auth_window.wait_window()
-    return code
+    def getUserData(self):
+        tk.Label(self.login_window, text="Username").pack()
+        username_box = tk.Entry(self.login_window)
+        username_box.pack()
+        tk.Label(self.login_window, text="Password").pack()
+        password_box = tk.Entry(self.login_window)
+        password_box.pack()
+        tk.Button(self.login_window, text="Save", command=self.login).pack()
+        return username_box.get(), password_box.get()
 
-def showSuccessWindow():
-    success_window = tk.Toplevel(login_window)
-    success_window.title("Success!")
-    success_window.geometry("200x200")
-    tk.Label(success_window, text="Logged in successfully!").pack()
-    tk.Button(success_window, text="Done", command= lambda: [success_window.destroy(), login_window.destroy()]).pack()
+    def login(self):
+        username, password = self.getUserData()
+        try:
+            self.L.load_session_from_file(username)
+        except FileNotFoundError:
+            try:
+                self.L.login(username, password) # Should prompt for 2FA
+                self.L.save_session_to_file()
+            except instaloader.exceptions.TwoFactorAuthRequiredException:
+                code = self.twoFactorAuthCode()
+                self.L.two_factor_login(code)
+                self.L.save_session_to_file()
+            
+        self.showSuccessWindow()
 
-login_window = tk.Tk()
-login_window.title("Log-in")
-login_window.geometry("300x300")
-tk.Label(login_window, text="Username").pack()
-username_box = tk.Entry(login_window)
-username_box.pack()
-tk.Label(login_window, text="Password").pack()
-password_box = tk.Entry(login_window)
-password_box.pack()
-tk.Button(login_window, text="Save", command=login).pack()
+    def twoFactorAuthCode(self):
+        code = None
+        auth_window = tk.Toplevel(self.login_window)
+        auth_window.title("Two Factor Authentication")
+        auth_window.geometry("250x300")
+        tk.Label(auth_window, text="Enter code").pack()
+        code_box = tk.Entry(auth_window)
+        code_box.pack()
+        def getCode():
+            nonlocal code
+            code = code_box.get()
+            auth_window.destroy()
+        
+        tk.Button(auth_window, text="Send", command=getCode).pack()
+        auth_window.wait_window()
+        return code
 
-login_window.mainloop()
+    def showSuccessWindow(self):
+        success_window = tk.Toplevel(self.login_window)
+        success_window.title("Success!")
+        success_window.geometry("200x200")
+        tk.Label(success_window, text="Logged in successfully!").pack()
+        tk.Button(success_window, text="Done", command= lambda: [success_window.destroy(), self.login_window.destroy()]).pack()
+        self.login_window.mainloop()
 
-L = instaloader.Instaloader()
+
 
 
 
